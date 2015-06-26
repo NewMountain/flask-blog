657ea7278565c52cfd43475ecf7cfc9b7a75d5ab
Christopher Nyberg <christopher.nyberg@gmail.com>
2015-06-25 23:31:03 -0700

    Added login screen
    Login screen added

--- blog.py
+++ blog.py
@@ -4,9 +4,13 @@
 from flask import Flask, render_template, request, session, \
 	flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 #Configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = ';\xf2\xed\x81\xd9\xe0\xe8\xf2<L-\xc5\xb7\xd10\x98\x85v\x93/V\x10\\\xec'
 
 app = Flask(__name__)
 
@@ -16,13 +20,27 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connection(app.config['DATABASE_PATH'])
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def login():
-	return render_template('login.html')
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or \
+			request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again.'
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
 	return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in',None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
